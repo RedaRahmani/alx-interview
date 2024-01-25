cd0192a77eb1ad67071d3bf05a22e8ff458ed072
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-01-25 23:04:01 +0100

    add log parsing file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,8 +7,10 @@ def print_msg(dict_sc, total_file_size):
     """
     Method to print
     Args:
-        dict of status codes
-        total of the file
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
     """
 
     print("File size: {}".format(total_file_size))
